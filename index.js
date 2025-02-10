af0bd7829beb595676641504ec50f4153f3841b0
durrealiyeva <durrealiyeva0@gmail.com>
2025-02-10 15:16:35 +0400

    modify

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const slider2 = document.querySelector(".sliderSecond")
 let show=true
 setInterval(()=>{
     show=!  show
-
     show ? slider1.style.transform = "translateX(-100%)" :  slider1.style.transform = "translateX(0%)"
     show ? slider2.style.transform = "translateX(-100%)" :  slider2.style.transform = "translateX(0%)"
 },2000)
